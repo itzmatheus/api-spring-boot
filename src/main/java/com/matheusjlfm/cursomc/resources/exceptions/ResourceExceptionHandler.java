2e852e86712454294773d982fa058ebd4c5927e1
Matheus Leite <matheusjoselfm@outlook.com.br>
2018-06-30 00:20:06 -0300

    Criacao e mapeamento das entidades Estado e Cidade

--- src/main/java/com/matheusjlfm/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/matheusjlfm/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -13,7 +13,7 @@ import com.matheusjlfm.cursomc.services.exceptions.ObjectNotFoundException;
 public class ResourceExceptionHandler {
 
 	@ExceptionHandler(ObjectNotFoundException.class)
-	public ResponseEntity<StandardError> ObjectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
