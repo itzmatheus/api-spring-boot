53f4a3e5e07e7b9c210b664f7f54e14421ca93a0
Matheus Leite <matheusjoselfm@gmail.com>
2018-07-08 23:04:39 -0300

    Adicionando endoit/pedidos/{id} - Disponibilizado

---
+++ src/main/java/com/matheusjlfm/cursomc/resources/PedidoResource.java
@@ -0,0 +1,27 @@
+package com.matheusjlfm.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.matheusjlfm.cursomc.domain.Pedido;
+import com.matheusjlfm.cursomc.services.PedidoService;
+
+@RestController
+@RequestMapping(value="/pedidos")
+public class PedidoResource {
+
+	@Autowired
+	private PedidoService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<Pedido> find(@PathVariable Integer id){
+		Pedido obj = service.find(id);
+		
+		return ResponseEntity.ok().body(obj);
+	}
+	
+}
