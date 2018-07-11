bf3c5058fbdcaa58664661e964e5b96196ef2e02
Matheus Leite <matheusjoselfm@gmail.com>
2018-07-10 22:29:29 -0300

    Validacao sintatica com Bean Validation em Categoria

--- src/main/java/com/matheusjlfm/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/matheusjlfm/cursomc/dto/CategoriaDTO.java
@@ -2,15 +2,21 @@ package com.matheusjlfm.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.matheusjlfm.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=80, message="O Tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
-	//Algumas bibliotecas precisam de um construtor vazio
 	public CategoriaDTO() {
 		
 	}
--- src/main/java/com/matheusjlfm/cursomc/services/CategoriaService.java
+++ src/main/java/com/matheusjlfm/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.matheusjlfm.cursomc.domain.Categoria;
+import com.matheusjlfm.cursomc.dto.CategoriaDTO;
 import com.matheusjlfm.cursomc.repositories.CategoriaRepository;
 import com.matheusjlfm.cursomc.services.exceptions.DataIntegrityException;
 import com.matheusjlfm.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,5 +54,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDto(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
