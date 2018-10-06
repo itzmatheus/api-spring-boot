86ff7d3e39e8136192498f8c56cb740a85595cac
Matheus Leite <matheusjoselfm@gmail.com>
2018-10-06 11:17:56 -0300

    Ajustando e testando a delecao de Cliente
    liberando delecao de clientes relacionados a enderecos

--- src/main/java/com/matheusjlfm/cursomc/services/ClienteService.java
+++ src/main/java/com/matheusjlfm/cursomc/services/ClienteService.java
@@ -62,7 +62,7 @@ public class ClienteService {
 		try {
 			repo.deleteById(id);
 		}catch(DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possivel excluir porque há entidades relacionadas");
+			throw new DataIntegrityException("Não é possivel excluir porque há pedidos relacionados");
 		}
 	}
 	
